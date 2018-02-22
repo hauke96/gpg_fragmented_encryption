5390af015038e21693646a5801993d34815b8ae0
Hauke Stieler <mail@hauke-stieler.de>
2018-02-22 01:16:11 +0100

    Use index based loop for argument parsing

--- gpg_fragmented_encryption.sh
+++ gpg_fragmented_encryption.sh
@@ -1,7 +1,9 @@
-
 #!/bin/bash
 
+set -e
+
 # Default settings
+recipient=
 folder=
 output_folder=./output_gpgfe
 
@@ -22,40 +24,50 @@ Arguments:
 Options:
     -h, --help      Prints this help message
     -o, --output    Specify output folder.
-                    Default: ./output_gpgfe/
+                    Default: ./output_gpgfe
 
 For bugs, feature requests or questions: mail@hauke-stieler.de
 END_USAGE
 }
 
-while (($#))
+# Check if recipient is provided
+recipient=$1
+if [ -z $recipient ]
+then
+	echo "Provide a recipient as first argument"
+	exit 1
+fi
+
+# Check if folder is provided
+folder=$2
+if [ -z $folder ]
+then
+	echo "Provide an input folder as second argument"
+	exit 1
+fi
+
+for (( i=3; i<=$#; i++ ))
 do
-	case "$1" in
+	arg_i=${@:$i:1}   # get argument i
+	arg_j=${@:$i+1:1} # get argument i+1
+	case $arg_i in
 	-h|--help)
 		usage
 		exit 0
 		;;
 	-o)
-		output_folder="$2"
-		shift
+		output_folder="$arg_j"
 		;;
 	--output=*)
-		output_folder="${1#*=}"
-		shift
+		output_folder="${arg_i#*=}"
 		;;
 	*)
-		shift
+		echo "Unknown argument number $i: '$arg_i'"
 		;;
 	esac
 done
 
-# Check if folder is provided
-if [ -z $folder ]
-then
-	echo "Provide a folder with '-p FOLDER' or '--path=FOLDER'"
-	exit 1
-fi
-
+echo "Recipient:         $recipient"
 echo "Folder to encrypt: $folder"
 echo "Output folder:     $output_folder"
 
